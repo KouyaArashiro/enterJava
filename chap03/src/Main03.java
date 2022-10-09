ed1dc61052bc4be267bb29928fb55de7e246d5dd
KouyaArashiro <a.kouya2001@gmail.com>
2022-10-09 22:35:13 +0900

    create chap03

---
+++ chap03/src/Main03.java
@@ -0,0 +1,18 @@
+public class Main03 {
+	public static void main(String[] args) {
+		
+	    int isHungry = 1;
+	    String food = "rice ball";
+	    System.out.println("Hello");
+	
+	    if(isHungry == 0) {
+	    	System.out.println("I'm full.");
+	    } else {
+		    System.out.println("I'm hungry.");
+		    System.out.println("I'm eating " + food);
+	    }
+	
+	    System.out.println("Thank you");
+      
+  }
+}
