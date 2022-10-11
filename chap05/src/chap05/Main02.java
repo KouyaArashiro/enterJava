44caa0cc87a03e492b13e80bb5ef609843ce5add
KouyaArashiro <a.kouya2001@gmail.com>
2022-10-11 21:01:11 +0900

    create chap05

---
+++ chap05/src/chap05/Main02.java
@@ -0,0 +1,15 @@
+package chap05;
+
+public class Main02 {
+
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出します");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
+
+	public static void hello() {
+		System.out.println("Hello");
+		
+	}
+}
