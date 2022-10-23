3df4b28726126ba3b3d3f49711517618b928bd34
KouyaArashiro <a.kouya2001@gmail.com>
2022-10-24 08:32:58 +0900

    learn 9-1

--- chapter/src/chap08/Main.java
+++ chapter/src/chap08/Main.java
@@ -3,16 +3,30 @@ package chap08;
 public class Main {
 
 	public static void main(String[] args) {
-		Hero h1 = new Hero("Arashiro", 200);
-		Hero h2 = new Hero("Koo");
-		Hero h3 = new Hero();
+//		Hero h1 = new Hero("Arashiro", 200);
+//		Hero h2 = new Hero("Koo");
+//		Hero h3 = new Hero();
 		
-		System.out.println(h1.name);
-		System.out.println(h1.hp);
-		System.out.println(h2.name);
-		System.out.println(h2.hp);
-		System.out.println(h3.name);
-		System.out.println(h3.hp);
+		Thief t1 = new Thief("t1", 100, 100);
+		Thief t2 = new Thief("t2", 100);
+		Thief t3 = new Thief("t3");
+		
+		System.out.println(t1.name);
+		System.out.println(t1.hp);
+		System.out.println(t1.mp);
+		System.out.println(t2.name);
+		System.out.println(t2.hp);
+		System.out.println(t2.mp);
+		System.out.println(t3.name);
+		System.out.println(t3.hp);
+		System.out.println(t3.mp);
+		
+//		System.out.println(h1.name);
+//		System.out.println(h1.hp);
+//		System.out.println(h2.name);
+//		System.out.println(h2.hp);
+//		System.out.println(h3.name);
+//		System.out.println(h3.hp);
 		
 //		Wizard w = new Wizard();
 //		w.name = "wiz";
