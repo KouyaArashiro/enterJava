584ed9e3088b59981c05f018bae8df2f3969151d
KouyaArashiro <a.kouya2001@gmail.com>
2022-12-29 09:24:53 +0900

    add Max3.java

---
+++ Algorithm/src/Max3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Max3 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		int a, b, c, max;
+		
+		System.out.println("３つの整数の最大値を求めます。");
+		System.out.print("aの値: ");
+		a = stdIn.nextInt();
+		System.out.print("bの値: ");
+		b = stdIn.nextInt();
+		System.out.print("cの値: ");
+		c = stdIn.nextInt();
+		
+		max = a;
+		
+		if(b > max) {
+			max = b;
+		} else if(c > max) {
+			max = c;
+		}
+		
+		System.out.println("最大値は" + max + "です");
+		
+		stdIn.close();
+		
+	}
+
+}
