e53a0c2398c79f4d955d4493243301717419818b
KouyaArashiro <a.kouya2001@gmail.com>
2022-12-29 10:12:36 +0900

    Add Max4Method.java

--- Algorithm/src/Max4Method.java
+++ Algorithm/src/Max4Method.java
@@ -5,18 +5,6 @@ public class Max4Method {
 		System.out.println("max3(4, 3, 2, 1) = " + max4(4, 3, 2, 1));
 		System.out.println("max3(4, 3, 2, 1) = " + max4(4, 3, 2, 4));
 		System.out.println("max3(4, 3, 2, 1) = " + max4(1, 3, 2, 4));
-//		System.out.println("max3(3, 2, 2) = " + max3(3, 2, 2));
-//		System.out.println("max3(3, 1, 2) = " + max3(3, 1, 2));
-//		System.out.println("max3(3, 2, 3) = " + max3(3, 2, 3));
-//		System.out.println("max3(2, 1, 3) = " + max3(2, 1, 3));
-//		System.out.println("max3(3, 3, 2) = " + max3(3, 3, 2));
-//		System.out.println("max3(3, 3, 3) = " + max3(3, 3, 3));
-//		System.out.println("max3(2, 2, 3) = " + max3(2, 2, 3));
-//		System.out.println("max3(2, 3, 1) = " + max3(2, 3, 1));
-//		System.out.println("max3(2, 3, 2) = " + max3(2, 3, 2));
-//		System.out.println("max3(1, 3, 2) = " + max3(1, 3, 2));
-//		System.out.println("max3(2, 3, 3) = " + max3(2, 3, 3));
-//		System.out.println("max3(1, 2, 3) = " + max3(1, 2, 3));
 	}
 	
 	static int max4(int a, int b, int c, int d) {
